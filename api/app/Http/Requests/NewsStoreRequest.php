3480c0e18e7d2f097706f9e7a2846a7eec394e92
murilo-carazato <2311600157@fema.edu.br>
2025-07-07 11:47:58 -0300

    feat(news): adicionar request de criação de notícia

---
+++ api/app/Http/Requests/NewsStoreRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class NewsStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required','string','min:5','max:255'],
+            'content' => ['required','string','min:15'],
+            'image' => ['image','mimes:jpeg,png,jpg'],
+            'type' => ['required', 'in:campaing,emergency'],
+            'user_id' => ['exists:users,id'],
+        ];
+    }
+}
