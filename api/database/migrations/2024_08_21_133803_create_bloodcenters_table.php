0f0dbe19d8231a78f0412c0b97132f6a988fee94
murilo-carazato <2311600157@fema.edu.br>
2025-07-07 11:48:00 -0300

    feat(migration): criar tabela bloodcenters

---
+++ api/database/migrations/2024_08_21_133803_create_bloodcenters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bloodcenters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('phone_number');
+            $table->float('latitude');
+            $table->float('longitude');
+            $table->string('email')->nullable()->unique();
+            $table->string('site')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bloodcenters');
+    }
+};
