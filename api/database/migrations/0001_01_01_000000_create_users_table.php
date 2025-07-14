b7528bf3b72e700178677030fe36ba172cfe83d0
murilo-carazato <2311600157@fema.edu.br>
2025-07-14 14:40:53 -0300

    feat(usuarios): adicionar campo isadmin e relacionamento com hemocentros

--- api/database/migrations/0001_01_01_000000_create_users_table.php
+++ api/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->enum('isadmin',['superadmin', 'admin']);
             $table->string('email')->unique();
             $table->foreignId('bloodcenter_id')->nullable();
