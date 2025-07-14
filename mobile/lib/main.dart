272c75ebf683995e29e4a1099fb2608f67b133d6
murilo-carazato <2311600157@fema.edu.br>
2025-07-14 15:10:08 -0300

    feat: atualizar entrypoint principal do app

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,125 +1,73 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:vitalink/services/helpers/database_helper.dart';
+import 'package:vitalink/services/models/user_model.dart';
+import 'package:vitalink/services/repositories/api/auth_repository.dart';
+import 'package:vitalink/services/repositories/api/blood_center_repository.dart';
+import 'package:vitalink/services/repositories/api/donation_repository.dart';
+import 'package:vitalink/services/repositories/user_repository.dart';
+import 'package:vitalink/services/stores/blood_center_store.dart';
+import 'package:vitalink/services/stores/auth_store.dart';
+import 'package:vitalink/services/stores/donation_store.dart';
+import 'package:vitalink/services/stores/nearby_store.dart';
+import 'package:vitalink/services/stores/user_store.dart';
+import 'package:vitalink/src/app.dart';
+import 'package:provider/provider.dart';
+import 'src/settings/settings_controller.dart';
+import 'src/settings/settings_service.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseMessaging.instance.requestPermission();
+  
+  final settingsController = SettingsController(SettingsService());
+  await settingsController.loadSettings();
+  //App é mantido no modo retrato
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  //Cria instância para database
+  await DatabaseHelper.instance.database;
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  //Instancia do repositório de usuário
+  UserRepository userRepository = UserRepository();
+  AuthRepository authRepository = AuthRepository();
+
+  final authStore = AuthStore(
+    authRepository: authRepository,
+    userRepository: userRepository,
+  );
+
+  //Captura se existe usuário salvo no banco de dados
+  List<UserModel> users = await userRepository.getUser();
+  UserStore userStore = UserStore(repository: userRepository);
+  if (users.isEmpty) {
+    //Cria usuário padrão se não houver dados salvos
+    userStore.state.value = [
+      UserModel(id: 1, name: 'Usuário', birthDate: '25/05/2002', bloodType: 'O-', hasMicropigmentation: false, hasPermanentMakeup: false, hasTattoo: false, viewedTutorial: false)
+    ];
+    await userRepository.createUser(userStore.state.value.first);
+  } else {
+    userStore.state.value = users;
   }
-}
+  final bloodCenterStore = BloodCenterStore(repository: BloodRepository());
+  final donationStore = DonationStore(repository: DonationRepository());
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => authStore),
+      ChangeNotifierProvider(create: (_) => userStore),
+      ChangeNotifierProvider(create: (_) => bloodCenterStore),
+      ChangeNotifierProvider(create: (_) => NearbyStore()),
+      ChangeNotifierProvider(create: (_) => donationStore),
+    ],
+    child: MyApp(
+      settingsController: settingsController,
+      userStore: userStore,
+      bloodCenterStore: bloodCenterStore,
+      donationStore: donationStore,
+    ),
+  ));
 }
