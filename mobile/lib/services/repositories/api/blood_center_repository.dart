44aaf48d1b9bd1a62fed151e60725ebef31ba1da
murilo-carazato <2311600157@fema.edu.br>
2025-07-14 15:07:40 -0300

    feat(repository): adicionar repositório de hemocentro

---
+++ mobile/lib/services/repositories/api/blood_center_repository.dart
@@ -0,0 +1,94 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:vitalink/services/helpers/http_client.dart';
+import 'package:vitalink/services/models/blood_center_model.dart';
+import 'package:vitalink/services/models/page_model.dart';
+
+abstract class IBloodRepository {
+  Future<List<BloodCenterModel>> index(
+      bool hasPagination,
+      ValueNotifier<int> page,
+      String search,
+      ValueNotifier<List<PageModel>> pages);
+}
+
+class BloodRepository implements IBloodRepository {
+  @override
+  Future<List<BloodCenterModel>> index(
+      bool hasPagination,
+      ValueNotifier<int> page,
+      String search,
+      ValueNotifier<List<PageModel>> pages) async {
+    try {
+      final response = await MyHttpClient.get(
+        url:
+            '/blood-center?has_pagination=$hasPagination&page=${page.value}&search=$search',
+        headers: MyHttpClient.getHeaders(token: MyHttpClient.token),
+      );
+      if (response.statusCode == 200) {
+        List<BloodCenterModel> bloodCenters = [];
+        List<PageModel> localPages = [];
+        final body = await jsonDecode(response.body);
+
+        //Tratamento para quando há paginação, pois o escopo do JSON é alterado
+        if (hasPagination) {
+          //Construção dos models de paginação
+          body['data']['links'].map((page) {
+            if (page['label'] is int) {
+              localPages.add(PageModel.fromMap(page as Map<String, dynamic>));
+            }
+          }).toList();
+
+          //Atualiza a lista de páginas - semelhante a notifyListeners() do ChangeNotifier
+          //Segue endereço de memória do parâmetro "pages" da função
+          pages.value = localPages;
+
+          //Contrução da listagem para quando há paginação
+          body['data']['data'].map((e) {
+            bloodCenters.add(BloodCenterModel.fromMap(e));
+          }).toList();
+
+          if (body['data'].containsKey('next_page_url')) {
+            if (body['data']['next_page_url'] != null) {
+              Uri uri = Uri.parse(body['data']['next_page_url']);
+              String? pageString = uri.queryParameters['page'];
+              int nextPage = int.parse(pageString!);
+              page.value = nextPage;
+            }
+          }
+        } else {
+          //Contrução da listagem para quando NÃO há paginação
+          body['data'].map((e) {
+            bloodCenters.add(BloodCenterModel.fromMap(e));
+          }).toList();
+        }
+
+        return bloodCenters;
+      } else if (response.statusCode == 404) {
+        return [];
+      } else {
+        throw response.reasonPhrase!;
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<BloodCenterModel> show(int id) async {
+    try {
+      final response = await MyHttpClient.get(
+        url: '/blood-center/$id',
+        headers: MyHttpClient.getHeaders(token: MyHttpClient.token),
+      );
+      if (response.statusCode == 200) {
+        final body = await jsonDecode(response.body);
+        return BloodCenterModel.fromMap(body['data']);
+      } else {
+        throw response.reasonPhrase!;
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
