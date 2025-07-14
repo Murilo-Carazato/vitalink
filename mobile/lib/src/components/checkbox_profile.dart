4af1167e2237d218c0b317d706d0b7c964bf5542
murilo-carazato <2311600157@fema.edu.br>
2025-07-14 15:07:43 -0300

    feat(components): adicionar checkbox de perfil

---
+++ mobile/lib/src/components/checkbox_profile.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:vitalink/styles.dart';
+
+class CheckBoxProfile extends StatefulWidget {
+  final String label;
+  final bool option;
+  final void Function(bool?)? onChanged;
+  const CheckBoxProfile({super.key, required this.option, required this.label, required this.onChanged});
+
+  @override
+  State<CheckBoxProfile> createState() => _CheckBoxProfileState();
+}
+
+class _CheckBoxProfileState extends State<CheckBoxProfile> {
+  @override
+  Widget build(BuildContext context) {
+    var labelStyle = Theme.of(context).inputDecorationTheme.labelStyle!.copyWith(fontSize: 18);
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Checkbox.adaptive(value: widget.option, onChanged: widget.onChanged),
+        RichText(
+          text: TextSpan(children: [
+            TextSpan(text: widget.label, style: labelStyle),
+            TextSpan(text: '*', style: labelStyle.copyWith(color: Styles.primary)),
+          ]),
+        ),
+        // Text(widget.label, style: Theme.of(context).textTheme.labelMedium)
+      ],
+    );
+  }
+}
