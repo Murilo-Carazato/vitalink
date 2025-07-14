37372f376a360faf37a2cd25c95a35cab15a8a21
murilo-carazato <2311600157@fema.edu.br>
2025-07-14 15:07:45 -0300

    feat(styles): adicionar estilos globais

---
+++ mobile/lib/styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Styles{
+  static const Color primary = Color.fromRGBO(237, 28, 36, 1);
+  static const Color border = Color.fromRGBO(231, 231, 231, 1);
+  static const Color darkBorder = Color.fromRGBO(24, 24, 24, 1);
+  static const Color green = Color.fromRGBO(31, 170, 8, 1);
+  static const Color gray1 = Color.fromRGBO(81, 80, 82, 1);
+  static const Color gray2 = Color.fromRGBO(51, 49, 56, 1);
+  static const Color darkBackground = Color.fromARGB(255, 27, 27, 29);
+}
