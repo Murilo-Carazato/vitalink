cd21c0ec8e36b3130301bf29978838cd832f781f
murilo-carazato <2311600157@fema.edu.br>
2025-07-15 00:18:45 -0300

    feat(api): registrar policies de User e BloodCenter no AuthServiceProvider

---
+++ api/app/Providers/AuthServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\BloodCenter;
+use App\Models\User;
+use App\Policies\BloodCenterPolicy;
+use App\Policies\UserPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        BloodCenter::class => BloodCenterPolicy::class,
+        User::class => UserPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
