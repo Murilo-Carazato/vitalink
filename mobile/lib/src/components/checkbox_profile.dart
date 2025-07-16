1dabcf52a8b2e484e72d7032fe7943e11b1e5c68
murilo-carazato <2311600157@fema.edu.br>
2025-07-16 12:32:51 -0300

    refactor(components): garantir uso seguro de estilos do tema em CheckBoxProfile

--- mobile/lib/src/components/checkbox_profile.dart
+++ mobile/lib/src/components/checkbox_profile.dart
@@ -14,7 +14,9 @@ class CheckBoxProfile extends StatefulWidget {
 class _CheckBoxProfileState extends State<CheckBoxProfile> {
   @override
   Widget build(BuildContext context) {
+    // Agora podemos usar diretamente o estilo do tema, sem verificações de nulo
     var labelStyle = Theme.of(context).inputDecorationTheme.labelStyle!.copyWith(fontSize: 18);
+        
     return Row(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -26,7 +28,6 @@ class _CheckBoxProfileState extends State<CheckBoxProfile> {
             TextSpan(text: '*', style: labelStyle.copyWith(color: Styles.primary)),
           ]),
         ),
-        // Text(widget.label, style: Theme.of(context).textTheme.labelMedium)
       ],
     );
   }
