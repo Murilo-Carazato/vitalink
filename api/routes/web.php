2144cfff6d63c245146035706d6bebc42339520f
murilo-carazato <2311600157@fema.edu.br>
2025-07-17 01:28:42 -0300

    chore(routes): adicionar rota dummy para login via deep link

--- api/routes/web.php
+++ api/routes/web.php
@@ -6,4 +6,9 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
+// Dummy GET /login to avoid 405 for deep link fallback
+Route::get('/login', function () {
+    return response()->json(['message' => 'Login via API only.'], 200);
+});
+
 require __DIR__.'/auth.php';
