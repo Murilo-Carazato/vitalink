c5cf20b1c0ad13be2c7e2ed2e0371b57cebd3c9b
murilo-carazato <2311600157@fema.edu.br>
2025-07-28 12:05:16 -0300

    chore(bootstrap): atualizar inicialização do app

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -18,6 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
+            'validate.donation.interval' => \App\Http\Middleware\ValidateDonationInterval::class,
+            'refresh.token' => \App\Http\Middleware\RefreshTokenMiddleware::class,
         ]);
 
         //
